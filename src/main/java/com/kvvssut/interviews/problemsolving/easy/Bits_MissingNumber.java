6b1c995316adce8c4e336e5c89004993df09b5b6
srimantasahu <srimant.sahu@gmail.com>
2022-03-13 02:35:23 +0530

    added Bits problems

---
+++ src/main/java/com/kvvssut/interviews/problemsolving/easy/Bits_MissingNumber.java
@@ -0,0 +1,25 @@
+package com.kvvssut.interviews.problemsolving.easy;
+
+/*
+    Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+    Input: nums = [9,6,4,2,3,5,7,0,1]
+    Output: 8
+    Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+*/
+
+public class Bits_MissingNumber {
+
+    public int missingNumber(int[] nums) {
+        int res = 0;
+        int i = 0;
+        for (; i < nums.length; i++) {
+            res ^= nums[i];
+            res ^= i;
+        }
+        res ^= i;
+
+        return res;
+    }
+
+}
