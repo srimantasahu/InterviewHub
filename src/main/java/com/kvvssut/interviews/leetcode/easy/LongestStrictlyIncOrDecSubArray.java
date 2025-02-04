f38117d357479e19eaecc8271729fa46621c9ae6
srimantasahu <srimant.sahu@gmail.com>
2025-02-04 11:11:43 +0530

    WIP: rotate array

---
+++ src/main/java/com/kvvssut/interviews/leetcode/easy/LongestStrictlyIncOrDecSubArray.java
@@ -0,0 +1,61 @@
+package com.kvvssut.interviews.leetcode.easy;
+
+/*
+You are given an array of integers nums. Return the length of the longest subarray of nums which is either strictly increasing or strictly decreasing.
+
+Example 1:
+Input: nums = [1,4,3,3,2]
+Output: 2
+Explanation:
+The strictly increasing subarrays of nums are [1], [2], [3], [3], [4], and [1,4].
+The strictly decreasing subarrays of nums are [1], [2], [3], [3], [4], [3,2], and [4,3].
+Hence, we return 2.
+
+Example 2:
+Input: nums = [3,3,3,3]
+Output: 1
+Explanation:
+The strictly increasing subarrays of nums are [3], [3], [3], and [3].
+The strictly decreasing subarrays of nums are [3], [3], [3], and [3].
+Hence, we return 1.
+
+Example 3:
+Input: nums = [3,2,1]
+Output: 3
+Explanation:
+The strictly increasing subarrays of nums are [3], [2], and [1].
+The strictly decreasing subarrays of nums are [3], [2], [1], [3,2], [2,1], and [3,2,1].
+Hence, we return 3.
+
+Constraints:
+1 <= nums.length <= 50
+1 <= nums[i] <= 50
+ */
+public class LongestStrictlyIncOrDecSubArray {
+
+    public static void main(String[] args) {
+        System.out.println(new LongestStrictlyIncOrDecSubArray().longestMonotonicSubArray(new int[]{1, 4, 3, 3, 2}));
+    }
+
+    public int longestMonotonicSubArray(int[] nums) {
+        int incLen = 1, decLen = 1, maxLen = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                incLen++;
+                decLen = 1;
+            } else if (nums[i] < nums[i - 1]) {
+                decLen++;
+                incLen = 1;
+            } else {
+                incLen = 1;
+                decLen = 1;
+            }
+
+            maxLen = Math.max(maxLen, Math.max(incLen, decLen));
+        }
+
+        return maxLen;
+    }
+
+}
