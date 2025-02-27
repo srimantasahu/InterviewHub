3f07c5eb59d1c6b0ad42308b87104a71f62a5e5d
srimantasahu <srimant.sahu@gmail.com>
2025-02-27 14:40:53 +0530

    added efficient code for sliding window maximum

--- src/main/java/com/kvvssut/interviews/leetcode/hard/SlidingWindowMaximum.java
+++ src/main/java/com/kvvssut/interviews/leetcode/hard/SlidingWindowMaximum.java
@@ -7,45 +7,57 @@ import java.util.PriorityQueue;
 class SlidingWindowMaximum {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(new SlidingWindowMaximum().maxSlidingWindow(new int[]{1, 3, 1, 2, 0, 5}, 3)));
+        System.out.println(Arrays.toString(new SlidingWindowMaximum().maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3)));
     }
 
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] ans = new int[n - k + 1];
+
+        for (int i = 0; i < n; i++) {
+            if (i % k == 0)
+                left[i] = nums[i];
+            else
+                left[i] = Math.max(left[i - 1], nums[i]);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (i == n - 1 || (i + 1) % k == 0)
+                right[i] = nums[i];
+            else
+                right[i] = Math.max(right[i + 1], nums[i]);
+        }
+
+        for (int i = 0; i <= n - k; i++) {
+            ans[i] = Math.max(right[i], left[i + k - 1]);
+        }
+
+        return ans;
+    }
+
+    public int[] maxSlidingWindowUsingPriorityQueue(int[] nums, int k) {
+        int n = nums.length;
         int[] res = new int[n - k + 1];
-        PriorityQueue<Record> pq = new PriorityQueue<>(Comparator.comparing(Record::getVal).thenComparing(Record::getIdx).reversed());
+        PriorityQueue<Entry> pq = new PriorityQueue<>(Comparator.comparing(Entry::val).thenComparing(Entry::idx).reversed());
 
         for (int i = 0; i < k; i++) {
-            pq.offer(new Record(nums[i], i));
+            pq.offer(new Entry(nums[i], i));
         }
-        res[0] = pq.peek().getVal();
+        res[0] = pq.peek().val;
 
         for (int i = k; i < n; i++) {
-            pq.offer(new Record(nums[i], i));
-            while (pq.peek().getIdx() <= i - k) {
+            pq.offer(new Entry(nums[i], i));
+            while (pq.peek().idx <= i - k) {
                 pq.poll();
             }
-            res[i - k + 1] = pq.peek().getVal();
+            res[i - k + 1] = pq.peek().val;
         }
 
         return res;
     }
 
-    private static class Record {
-        int val;
-        int idx;
-
-        public Record(int val, int idx) {
-            this.val = val;
-            this.idx = idx;
-        }
-
-        public int getVal() {
-            return val;
-        }
-
-        public int getIdx() {
-            return idx;
-        }
+    private record Entry(int val, int idx) {
     }
 }
